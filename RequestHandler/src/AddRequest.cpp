360b8da679b03774dc32884d6a715729ae4b602f
LazyPanda07 <semengricenko@gmail.com>
2021-04-06 12:47:16 +0500

    Added adding requests

--- RequestHandler/src/AddRequest.cpp
+++ RequestHandler/src/AddRequest.cpp
@@ -1,5 +1,12 @@
 #include "AddRequest.h"
 
+#include "Database/SP_destination.h"
+#include "Database/SP_reference.h"
+#include "Database/Mode.h"
+#include "Database/Constants.h"
+
+using namespace std;
+
 void AddRequest::init(const framework::utility::JSONSettingsParser::ExecutorSettings& settings)
 {
 
@@ -7,7 +14,42 @@ void AddRequest::init(const framework::utility::JSONSettingsParser::ExecutorSett
 
 void AddRequest::doPost(framework::HTTPRequest&& request, framework::HTTPResponse& response)
 {
+	try
+	{
+		string id = request.getAttribute("id");
+		const json::JSONParser& json = request.getJSON();
 
+		auto& spDestinationModel = request.getDatabaseModelInstance<db::SP_destination>(db::databaseName, db::spDestinationTableName);
+		auto& spReferenceModel = request.getDatabaseModelInstance<db::SP_reference>(db::databaseName, db::spReferenceTableName);
+		auto& modeModel = request.getDatabaseModelInstance<db::Mode>(db::databaseName, db::modeTableName);
+
+		const string& destination = json.get<string>("destination");
+		const string& type = json.get<string>("type");
+
+		spDestinationModel->insertQuery({ { "destination", destination } });
+		spReferenceModel->insertQuery({ { "type", type } });
+
+		string destinationId = spDestinationModel->selectByFieldQuery({ { "destination", destination } })[0].at("id_destination");
+		string referenceId = spReferenceModel->selectByFieldQuery({ { "type", type } })[0].at("id_reference");
+
+		modeModel->insertQuery({ { "id_reference", referenceId }, { "id_destination", destinationId }, { "id_user", id } });
+
+		response.addBody
+		(
+			json::JSONBuilder(1251).push_back<bool>(make_pair("success", true))
+		);
+	}
+	catch (const out_of_range&)
+	{
+		response.setResponseCode(web::ResponseCodes::unauthorized);
+	}
+	catch (const runtime_error& e)
+	{
+		response.addBody
+		(
+			json::JSONBuilder(1251).push_back<bool>(make_pair("success", false))
+		);
+	}
 }
 
 void AddRequest::destroy()
